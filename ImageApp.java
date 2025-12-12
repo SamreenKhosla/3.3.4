2a5145a78fc0ff427a4cad2b17e01061fa9af778
IshitaPrasad1 <prasad1449@mydusd.org>
2025-12-11 23:17:44 -0800

    Added comments to code

--- ImageApp.java
+++ ImageApp.java
@@ -7,15 +7,20 @@ public class ImageApp {
 
     String pictureFile = "lib/beach.jpg";
 
+    // Load the original image from library folder
     Picture origImg = new Picture(pictureFile);
     Pixel[][] origPixels = origImg.getPixels2D();
+    //Print color of top-left pixel for debugging
     System.out.println(origPixels[0][0].getColor());
+    //open a window to explore original image 
     origImg.explore();
 
     boolean running = true;
 
+    //Main loop that keeps running until user chooses to quit
     while (running) {
 
+      //show menu and get user choice
       String choice = showMenu();
 
       switch (choice) { 
@@ -49,6 +54,7 @@ public class ImageApp {
           break;
 
         case "Quit":
+          //exit the loop and end program
           running = false;
           break;
       }
@@ -56,6 +62,7 @@ public class ImageApp {
   }
 
 
+  //Displays a menu using JOptionPane and returns the user's choice
   public static String showMenu() {
 
     String[] ops = {
@@ -84,6 +91,7 @@ public class ImageApp {
   }
 
 
+  //Recolors the image by swapping RGB to BRG
   public static void doRecolor(String file) {
     Picture img = new Picture(file);
     Pixel[][] pixels = img.getPixels2D();
@@ -91,6 +99,7 @@ public class ImageApp {
     for (Pixel[] row : pixels)
       for (Pixel p : row) {
         int r = p.getRed(), g = p.getGreen(), b = p.getBlue();
+        // Swap Red and Blue
         p.setRed(b);
         p.setGreen(r);
         p.setBlue(g);
@@ -100,12 +109,14 @@ public class ImageApp {
     img.explore();
   }
 
+  //Creates a negative of the image by inverting RGB values
   public static void doNegative(String file) {
     Picture img = new Picture(file);
     Pixel[][] pixels = img.getPixels2D();
 
     for (Pixel[] row : pixels)
       for (Pixel p : row) {
+    //` Invert each color component
         p.setRed(255 - p.getRed());
         p.setGreen(255 - p.getGreen());
         p.setBlue(255 - p.getBlue());
@@ -115,12 +126,15 @@ public class ImageApp {
     img.explore();
   }
 
+  //Converts the image to grayscale by averaging RGB values
   public static void doGrayscale(String file) {
     Picture img = new Picture(file);
     Pixel[][] pixels = img.getPixels2D();
 
     for (Pixel[] row : pixels)
       for (Pixel p : row) {
+
+        // Calculate average of RGB components
         int avg = (p.getRed() + p.getGreen() + p.getBlue()) / 3;
         p.setRed(avg);
         p.setGreen(avg);
@@ -131,15 +145,18 @@ public class ImageApp {
     img.explore();
   }
 
+  //Rotates the image 180 degrees
   public static void doRotate180(String file) {
     Picture orig = new Picture(file);
     Pixel[][] src = orig.getPixels2D();
 
+    // Create new picture with swapped dimensions
     Picture out = new Picture(orig.getHeight(), orig.getWidth());
     Pixel[][] dest = out.getPixels2D();
 
     int h = src.length, w = src[0].length;
 
+    // copy pixels in reverse order to rotate 180 degrees
     for (int r = 0; r < h; r++)
       for (int c = 0; c < w; c++)
         dest[h - 1 - r][w - 1 - c].setColor(src[r][c].getColor());
@@ -148,6 +165,7 @@ public class ImageApp {
     out.explore();
   }
 
+  //Rotates the image 90 degrees clockwise
   public static void doRotate90CW(String file) {
     Picture orig = new Picture(file);
     Pixel[][] src = orig.getPixels2D();
@@ -156,6 +174,7 @@ public class ImageApp {
     Picture out = new Picture(w, h);
     Pixel[][] dest = out.getPixels2D();
 
+    // assigns pixels to new rotated positions 
     for (int r = 0; r < h; r++)
       for (int c = 0; c < w; c++)
         dest[c][h - 1 - r].setColor(src[r][c].getColor());
@@ -164,14 +183,16 @@ public class ImageApp {
     out.explore();
   }
 
+  //Rotates the image 90 degrees counter-clockwise
   public static void doRotate90CCW(String file) {
     Picture orig = new Picture(file);
     Pixel[][] src = orig.getPixels2D();
 
     int h = src.length, w = src[0].length;
-    Picture out = new Picture(w, h);
+    Picture out = new Picture(w, h); //Swap width and height 
     Pixel[][] dest = out.getPixels2D();
 
+    // assigns pixels to new rotated positions
     for (int r = 0; r < h; r++)
       for (int c = 0; c < w; c++)
         dest[w - 1 - c][r].setColor(src[r][c].getColor());
@@ -180,6 +201,7 @@ public class ImageApp {
     out.explore();
   }
 
+  //Adds a sticker image onto the larger main image at a fixed position
   public static void doAddSticker() {
 
     Picture big = new Picture("lib/beach.jpg");
@@ -188,20 +210,22 @@ public class ImageApp {
     Pixel[][] bigPix = big.getPixels2D();
     Pixel[][] smPix = small.getPixels2D();
 
-    int startR = 50;
-    int startC = 50;
+    int startR = 50; //Starting row for sticker
+    int startC = 50; //Starting column for sticker
 
     for (int r = 0; r < smPix.length; r++)
       for (int c = 0; c < smPix[0].length; c++) {
 
         Color col = smPix[r][c].getColor();
 
+        // Skip white pixels (make background  transparent)
         if (col.getRed() == 255 && col.getGreen() == 255 && col.getBlue() == 255)
           continue;
 
         int br = startR + r;
         int bc = startC + c;
 
+        // only change pixels inside the bounds of the big image 
         if (br < bigPix.length && bc < bigPix[0].length)
           bigPix[br][bc].setColor(col);
       }
